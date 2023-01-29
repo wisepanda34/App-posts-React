c02bf112ca770c3993c9c9cce009e965438fd385
Yevgen Naidenko <evghen.naydenko@gmail.com>
2023-01-29 23:14:29 +0200

    finished app

--- my-app/src/components/app/app.js
+++ my-app/src/components/app/app.js
@@ -21,12 +21,16 @@ export default class App extends Component {
 				{ label: 'I need a break...', important: false, like: false, id: 3 }
 
 			],
-			term: []
+			term: '',
+			filter: 'all'
 		}
 		this.deleteItem = this.deleteItem.bind(this);
 		this.addItem = this.addItem.bind(this);
 		this.onToggleImportant = this.onToggleImportant.bind(this);
 		this.onToggleLiked = this.onToggleLiked.bind(this);
+		this.onUpdateSearch = this.onUpdateSearch.bind(this);
+		this.onFilterSelect = this.onFilterSelect.bind(this);
+
 
 		this.maxId = 4;
 	};
@@ -90,14 +94,29 @@ export default class App extends Component {
 		})
 	}
 
+	filterPost(items, filter) {
+		if (filter === 'like') {
+			return items.filter(item => item.like)
+		} else {
+			return items
+		}
+	}
+
+	onUpdateSearch(term) {
+		this.setState({ term: term });
+	}
+
+	onFilterSelect(filter) {
+		this.setState({ filter: filter });
+	}
 
 	render() {
-		const { data, term } = this.state;
+		const { data, term, filter } = this.state;
 
 		const liked = data.filter(item => item.like).length;
 		const allPosts = data.length;
 
-		const visiblePosts = this.searchPost(data, term);
+		const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
 		return (
 			<div className='app' >
@@ -105,8 +124,13 @@ export default class App extends Component {
 					liked={liked}
 					allPosts={allPosts} />
 				<div className='serch-panel d-flex'>
-					<SearchPanel />
-					<PostStatusFilter />
+					<SearchPanel
+						onUpdateSearch={this.onUpdateSearch}
+					/>
+					<PostStatusFilter
+						filter={filter}
+						onFilterSelect={this.onFilterSelect}
+					/>
 				</div>
 				<PostList
 					posts={visiblePosts}
--- my-app/src/components/search-panel/search-panel.js
+++ my-app/src/components/search-panel/search-panel.js
@@ -7,14 +7,21 @@ export default class SearchPanel extends Component {
 		this.state = {
 			term: ''
 		}
-
+		this.onUpdateSearch = this.onUpdateSearch.bind(this);
 	}
+	onUpdateSearch(e) {
+		const term = e.target.value;
+		this.setState({ term: term });
+		this.props.onUpdateSearch(term);
+	}
+
 	render() {
 		return (
 			<input
 				className='form-control search-input'
 				type='text'
 				placeholder='Search by notes'
+				onChange={this.onUpdateSearch}
 			/>
 		)
 	}
